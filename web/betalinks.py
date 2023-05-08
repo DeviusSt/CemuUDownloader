4eee1409fd67caee62d5427738e30e84582cc679
Synth <110778109+SynthouS@users.noreply.github.com>
2023-05-08 17:11:57 +0500

    Create betalinks.py

---
+++ web/betalinks.py
@@ -0,0 +1,19 @@
+rom tkinter import *
+import webbrowser
+
+root = Tk()
+
+def btn_click():
+    webbrowser.open('https://github.com/SynthouS/WiiUScreen2Cemu/releases/download/apps/WiiUScreen2Cemu.zip', new=2)
+
+def btn_click2():
+    webbrowser.open('https://github.com/SynthouS/WiiUScreen2Cemu/releases/download/v5.5.2/WiiUApps2CemuUSA.zip', new=2)
+
+def btn_click3():
+    webbrowser.open('https://github.com/TheSipod/Splatoon-NoMotion/files/9280968/SplatoonFIX.zip', new=2)
+
+def btn_click4():
+    webbrowser.open('https://t.me/SynthouS', new=2)
+
+def btn_click5():
+    webbrowser.open('https://github.com/SynthouS', new=2)
